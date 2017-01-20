e797f89d842a476f7f0841aceddaae3593127f54
mn14tm <mn14tm@leeds.ac.uk>
2017-01-20 15:14:20 +0000

    turned into package

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='lifetime',
-      version='0.1',
-      description='Extracting lifetime from decays',
-      url='http://github.com/mn14tm/lifetime',
-      author='Thomas Mann',
-      author_email='mn14tm@leeds.ac.uk',
-      license='MIT',
-      packages='[lifetime]'
-      zip_safe=False)
+setup(
+    name='lifetime',  # This is the name of your PyPI-package.
+    version='0.1',  # Update the version number for new releases
+    description='Extracting lifetimes from decay measurements',
+    url='http://github.com/mn14tm/lifetime',
+    download_url='https://github.com/mn14tm/lifetime/tarball/0.1',
+    author='Thomas Mann',
+    author_email='mn14tm@leeds.ac.uk',
+    license='MIT',
+    keywords='fluorescence decay lifetime fitting',
+    packages=['lifetime'],
+    install_requires=['numpy', 'scipy']
+)
