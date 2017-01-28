eec0080f72c2a291e9a414e1f3cbbe0d604f9859
mn14tm <mn14tm@leeds.ac.uk>
2017-01-28 13:44:29 +0000

    increased norm specturm tails

--- lifetime/spectrum.py
+++ lifetime/spectrum.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 
-def normalise(x, y, tail_lower=1410, tail_upper=1690):
+def normalise(x, y, tail_lower=1430, tail_upper=1670, intensity=True):
     """Normalise x to maximum value after removing baseline noise from tail averages"""
     loc = np.where((x < tail_lower) | (x > tail_upper))
     y -= np.mean(y[loc])  # Subtract baseline from tails
     # Normalise
-    y /= max(y)
+    if intensity:
+        y /= max(y)
     return x, y
